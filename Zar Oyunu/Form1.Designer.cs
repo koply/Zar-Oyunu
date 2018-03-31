02bb5bde7b0eecfe84d98661dad9bc74549f3d2c
Musa Berat <musafg@outlook.com>
2018-03-31 19:43:08 +0300

    Uploading Files
    Main Files.

---
+++ Zar Oyunu/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Zar_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Code Pro Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(205, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0 - 0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(25, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(104, 224);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(32, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "1. Oyuncu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(412, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "2. Oyuncu";
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(405, 57);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(104, 224);
+            this.listBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(200, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 49);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Zar At";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(197, 131);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(133, 21);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Tek seferde bitir.";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.Location = new System.Drawing.Point(197, 154);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 21);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.Text = "Teker teker.";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(195, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "*10 defa çekilir.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Code Pro Black", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(205, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 41);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "0 - 0";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(520, 327);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(197, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            this.textBox1.MouseEnter += new System.EventHandler(this.textBox1_MouseEnter);
+            this.textBox1.MouseLeave += new System.EventHandler(this.textBox1_MouseLeave);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.Gray;
+            this.label7.Location = new System.Drawing.Point(222, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Kaç tur?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(524, 339);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zar Oyunu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Zar Oyunu/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zar_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+         //neyse   
+        }
+        int p1top = 0;
+        int p2top = 0;
+        int p11 = 0;
+        int p22 = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            p1top = 0;
+            p2top = 0;
+            if (radioButton1.Checked == true && radioButton2.Checked == true) { MessageBox.Show("Lütfen 1 tane çekim türü seçiniz", "Zar Oyunu", MessageBoxButtons.OK, MessageBoxIcon.Warning); }
+            else if (radioButton1.Checked == true)
+            {
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                if (textBox1.Text != "") { toplu(Convert.ToInt32(textBox1.Text)); }
+                else { toplu(10); }
+                if (p1top < p2top)
+                    MessageBox.Show("Kazanan 2.Oyuncu!!\nSonuç: 1.Oyuncu: " + p1top + "\n             2.Oyuncu: " + p2top, "Zar Oyunu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else if (p2top < p1top)
+                    MessageBox.Show("Kazanan 1.Oyuncu!!\nSonuç: 1.Oyuncu: " + p1top + "\n             2.Oyuncu: " + p2top, "Zar Oyunu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else if (p2top == p1top)
+                    MessageBox.Show("Kazanan Yoook!!\nSonuç: 1.Oyuncu: " + p1top + "\n             2.Oyuncu: " + p2top, "Zar Oyunu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (radioButton2.Checked == true)
+            {
+                teker();
+                p11 = p1top + p11;
+                p22 = p2top + p22;
+                label5.Text = p11 + " - " + p22;
+
+            }
+            else if (radioButton1.Checked == false && radioButton2.Checked == false) { radioButton1.Checked = true; toplu(10); }
+        }
+
+        void teker() {
+            Random random = new Random();
+            int p1point = random.Next(1, 6);
+            int p2point = random.Next(1, 6);
+            label1.Text = p1point + " - " + p2point;
+            listBox1.Items.Add(p1point);
+            listBox2.Items.Add(p2point);
+            p1top = p1point;
+            p2top = p2point;
+            
+        }
+
+        void toplu(int tekrar) {
+            
+            Random random = new Random();
+            for (int i = 0; i < tekrar; i++)
+            {
+                int p1point = random.Next(1, 6);
+                int p2point = random.Next(1, 6);
+                label1.Text = p1point + " - " + p2point;
+                listBox1.Items.Add(p1point);
+                listBox2.Items.Add(p2point);
+                p1top = p1top + p1point;
+                p2top = p2top + p2point;
+            }
+            label5.Text = p1top.ToString() + " - " + p2top.ToString();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true) { label4.Visible = true; label7.Visible = true; textBox1.Visible = true; }
+            else if (radioButton1.Checked == false) { label4.Visible = false; label7.Visible = false; textBox1.Visible = false; }
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            label1.Text = "0 - 0";
+            label5.Text = "0 - 0";
+            textBox1.Text = "";
+            p1top = 0;
+            p2top = 0;
+            p11 = 0;
+            p22 = 0;
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            label7.Visible = false;
+            label4.Visible = false;
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                label7.Visible = true;
+                label4.Visible = true;
+            }
+            else { label7.Visible = false; label4.Visible = false; }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+
+            label7.Visible = false;
+            label4.Visible = false;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                label7.Visible = true;
+                label4.Visible = true;
+            }
+            else { label7.Visible = false; label4.Visible = false; }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = char.IsLetter(e.KeyChar) || char.IsControl(e.KeyChar);
+            label7.Visible = false;
+            label4.Visible = false;
+            if (e.KeyChar == 8) {
+                if (textBox1.Text == "") { }
+                else
+                {
+                    object sayi;
+                    string son;
+
+                    sayi = textBox1.Text;
+
+                    son = textBox1.Text.Substring(0, textBox1.Text.Length - 1);
+
+                    textBox1.Text = son;
+                }
+            }
+
+        }
+    }
+}
